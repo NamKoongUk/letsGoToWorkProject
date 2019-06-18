9c758f143aca7365e7bc29815757df0032c09bfc
KhS85_000 <KhS85_000@khs0904>
2019-06-18 15:34:21 +0900

    2019-06-18 커뮤티니  01 파일

--- src/main/java/com/kh/lgtw/community/model/dao/CommunityDao.java
+++ src/main/java/com/kh/lgtw/community/model/dao/CommunityDao.java
@@ -1,5 +1,45 @@
 package com.kh.lgtw.community.model.dao;
 
+import org.mybatis.spring.SqlSessionTemplate;
+
+import com.kh.lgtw.community.model.vo.Community;
+import com.kh.lgtw.community.model.vo.CommunityAttachment;
+import com.kh.lgtw.community.model.vo.CommunityComment;
+import com.kh.lgtw.community.model.vo.CommunityPost;
+
 public interface CommunityDao {
+
+	Community SelectCommunity(SqlSessionTemplate sqlSession, Community cm);
+
+	int UpdateCommunity(SqlSessionTemplate sqlSession, Community cm);
+
+	int DelectCommunity(SqlSessionTemplate sqlSession, Community cm);
+
+	int InsertCommunity(SqlSessionTemplate sqlSession, Community cm);
+
+	CommunityPost SelectCommunityPost(SqlSessionTemplate sqlSession, CommunityPost cp);
+
+
+	int InsertCommunityPostAtt(SqlSessionTemplate sqlSession, CommunityAttachment ca);
+
+	int DelectCommunityPost(SqlSessionTemplate sqlSession, CommunityPost cp);
+
+	int DelectCommunityPostAtt(SqlSessionTemplate sqlSession, CommunityAttachment ca);
+
+	int UpdateCommunityPost(SqlSessionTemplate sqlSession, CommunityPost cp);
+
+	int UpdateCommunityPostAtt(SqlSessionTemplate sqlSession, CommunityAttachment ca);
+
+	int InsertCommunityPost(SqlSessionTemplate sqlSession, CommunityPost cp);
+
+	CommunityComment SelectCommunityComment(SqlSessionTemplate sqlSession, CommunityComment cc);
+
+	int insertCommunityComment(SqlSessionTemplate sqlSession, CommunityComment cc);
+
+	int DeleteCommunityComment(SqlSessionTemplate sqlSession, CommunityComment cc);
+
+	int UpdateCommunityComment(SqlSessionTemplate sqlSession, CommunityComment cc);
+
+	
 
 }
