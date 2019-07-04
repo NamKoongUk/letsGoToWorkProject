a2b327f4b2c7150ac37c9cd6cb72c11622b56187
cymki <cymki@DESKTOP-HPR4GB8>
2019-07-04 21:03:29 +0900

    메일 보내기는 됨

--- src/main/java/com/kh/lgtw/mail/controller/SendMail.java
+++ src/main/java/com/kh/lgtw/mail/controller/SendMail.java
@@ -10,13 +10,15 @@ import com.kh.lgtw.mail.model.vo.Sender;
 public class SendMail {
 	public void send (Sender sender) {
 		// 로깅처리해줘야함!
-		
+		System.out.println("sendMail 실행됨!!");
 		try {
 			ProfileCredentialsProvider credentialsProvider = new ProfileCredentialsProvider();
+			System.out.println("ProfileCredential생성 : " + credentialsProvider);
 			
 			try {
 				// 자격증명 가져오기 
 				credentialsProvider.getCredentials();
+				System.out.println("자격 증명 가져오기 : " + credentialsProvider.getCredentials());
 			} catch(Exception e) {
 				throw new AmazonClientException("자격증명을 가져오지 못했음!\n자격증명 파일의 위치와 포맷은 (~/.aws/credentials) : " + e);
 			}
